f5bfd1fab17d5fb2d48006863c771d691f209892
Bruno Martínez González <bruno.martinez@new-work.se>
2023-03-16 21:53:07 +0100

    feat: gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://www.kokualabs.com',
+    base: '/',
     integrations: [astroI18next()],
 });
