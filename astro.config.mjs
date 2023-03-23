1e7601b623778529fef0f7b456e4b19008e21892
Bruno Martínez González <bruno.martinez@new-work.se>
2023-03-23 15:20:47 +0100

    compress functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kokualabs.com",
-  integrations: [astroI18next()],
+  integrations: [astroI18next(), compress()]
 });
