b145affc299f20ec64f39e3966066a4e354db4a6
Bruno Martínez González <bruno132mg98@gmail.com>
2023-03-23 17:11:02 +0100

    refactor for optimization (#13)

---
+++ src/config/social.ts
@@ -0,0 +1,10 @@
+type SocialItem = {
+  to: string;
+  icon: string;
+};
+export const socialLinks: SocialItem[] = [
+  { to: "https://www.linkedin.com/company/kokua-labs/", icon: "linkedin" },
+  { to: "https://www.instagram.com/kokualabs/", icon: "instagram" },
+  { to: "/", icon: "facebook" },
+  { to: "mailto:hello@kokualabs.com", icon: "mail" },
+];
