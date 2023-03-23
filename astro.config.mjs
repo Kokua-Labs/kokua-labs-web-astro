2787f2a168d5812e7049f5bbb1e08db42c74cf58
Bruno Martínez González <bruno.martinez@new-work.se>
2023-03-23 17:09:25 +0100

    refactor for optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import compress from "astro-compress";
-
 import sitemap from "@astrojs/sitemap";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kokualabs.com",
-  integrations: [astroI18next(), compress(), sitemap()],
+  integrations: [astroI18next(), compress(), sitemap(), compressor({gzip: true})]
 });
