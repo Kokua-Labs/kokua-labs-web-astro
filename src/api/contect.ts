0b35b40d3a500d77c40d33a8fdd01a68f21271e6
Bruno Martínez González <bruno132mg98@gmail.com>
2023-04-08 17:58:44 +0200

    contact page (#29)

---
+++ src/api/contect.ts
@@ -0,0 +1,1 @@
+export const contact = async () => {};
---
+++ src/config/contact-topics.ts
@@ -0,0 +1,54 @@
+export const contactTopis: { [key: string]: { label: string; value: string }[] } = {
+  es: [
+    {
+      label: "Web",
+      value: "web",
+    },
+    {
+      label: "Eccomerce",
+      value: "Eccomerce",
+    },
+    {
+      label: "Apps",
+      value: "app",
+    },
+    {
+      label: "Seo",
+      value: "seo",
+    },
+    {
+      label: "Legal",
+      value: "legal",
+    },
+    {
+      label: "Otro",
+      value: "other",
+    },
+  ],
+  en: [
+    {
+      label: "Web",
+      value: "web",
+    },
+    {
+      label: "E-commerce",
+      value: "e-commerce",
+    },
+    {
+      label: "Apps",
+      value: "apps",
+    },
+    {
+      label: "SEO",
+      value: "seo",
+    },
+    {
+      label: "Legal",
+      value: "legal",
+    },
+    {
+      label: "Other",
+      value: "other",
+    },
+  ],
+};
