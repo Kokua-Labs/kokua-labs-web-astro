4320e9b6d449ec716df62f12a4c6804ebcd7c02c
Bruno Martínez González <bruno.martinez@new-work.se>
2023-04-17 13:19:15 +0200

    loading spinner

--- src/config/cookies.ts
+++ src/config/cookies.ts
@@ -10,16 +10,15 @@ type CookiesText = {
 export const cookiesTexts: CookiesText = {
   en: {
     title: "About our 🍪",
-    description:
-      "We use Cookies to facilitate navigation and provide a better web experience. ",
-    knowMore: "Learn more",
+    description: "We use Cookies to facilitate navigation and provide a better web experience. ",
+    knowMore: "Know more.",
     ctaLabel: "Accept",
   },
   es: {
     title: "Sobre nuestas 🍪",
     description:
-      "Hacemos uso de Cookies para facilitar la navegación y porder ofrecer una mejor experiencia en de la web. ",
-    knowMore: "Saber más",
+      "Hacemos uso de Cookies para facilitar la navegación y poder ofrecer una mejor experiencia en de la web. ",
+    knowMore: "Saber más.",
     ctaLabel: "Aceptar",
   },
 };
